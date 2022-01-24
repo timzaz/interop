51db75d58d58a8516f53557e2376dc6420531b0f
A. J. Wray <ajw@ajw.sl>
2022-01-24 13:13:42 +0000

    fix cli subscriber command

--- interop/cli/subscriber.py
+++ interop/cli/subscriber.py
@@ -35,24 +35,45 @@ class SubscriberCli(typer.Typer):
         """Scaffolds a new subscriber"""
 
         import_name = get_import_name()
+        default_module = f"{import_name}.subscribers"
 
         module: str = typer.prompt(
             "The module at whose directory the subscriber will be placed",
-            default=f"{import_name}.subscribers",
+            default=default_module,
         )
 
-        module_module: typing.Optional[
-            types.ModuleType
-        ] = importlib.import_module(module)
-        subscriber_dir: str = os.path.dirname(inspect.getfile(module_module))
+        subscriber_dir: str | None = None
+        try:
+            if module == default_module:
+                import_module: typing.Optional[
+                    types.ModuleType
+                ] = importlib.import_module(import_name)
+                subscriber_dir = os.path.join(
+                    os.path.dirname(inspect.getfile(import_module)),
+                    "subscribers"
+                )
 
-        if not os.path.exists(subscriber_dir):
-            os.mkdir(subscriber_dir)
+                if not os.path.exists(subscriber_dir):
+                    os.mkdir(subscriber_dir)
 
-            init_dir: str = os.path.join(subscriber_dir, "__init__.py")
-            if not os.path.exists(init_dir):
-                with open(init_dir, "w") as file:
-                    file.write("")
+                    init_dir: str = os.path.join(subscriber_dir, "__init__.py")
+                    if not os.path.exists(init_dir):
+                        with open(init_dir, "w") as file:
+                            file.write("")
+            else:
+                module_module: typing.Optional[
+                    types.ModuleType
+                ] = importlib.import_module(module)
+                subscriber_dir = os.path.dirname(
+                    inspect.getfile(module_module)
+                )
+        except:  # noqa
+            typer.echo(f"Could not import module: {module}\n\n")
+            raise typer.Abort()
+
+        if not subscriber_dir:
+            typer.echo("Could not deduce subscriber directory.\n\n")
+            raise typer.Abort()
 
         dir = subscriber_dir
         env = Environment()
